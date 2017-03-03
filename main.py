aded9bab07bab88b27c66314570991be0c1b7c44
Carlos Gomez <kurolox@gmail.com>
2017-03-03 10:34:26 +0100

    Added main program. Fixed credentials dict.

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+import pyperclip
+import imgurpython
+from credentials import credentials
+from datetime import datetime
+
+
+os.system("maim -s /tmp/temp.png")
+timenow = str(datetime.now())
+config = {
+        "album" : credentials["album_id"],
+        "description" : "Screenshot taken at " + timenow,
+        "name" : timenow}
+
+client = imgurpython.ImgurClient(credentials["client_id"], credentials["client_secret"],credentials["access_token"], credentials["refresh_token"])
+
+client.upload_from_path("/tmp/temp.png", config=config, anon=False)
+os.remove("/tmp/temp.png")
+album = client.get_album_images(credentials["album_id"])
+for image in album:
+    if image.description == config["description"]:
+        print(image.link)
+        pyperclip.copy(image.link)
+        break
+
