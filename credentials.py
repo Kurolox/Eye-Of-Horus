b9e6fe2c2e54e63b3f40b5fde3102d25599ffb43
Carlos Gomez <kurolox@gmail.com>
2017-03-03 10:16:03 +0100

    Added credentials form

---
+++ credentials.py
@@ -0,0 +1,7 @@
+# Just put your login credentials in this dict.
+
+credentials = {
+        "client_id" = "", #You can get your client ID by registering in the imgur api: https://api.imgur.com/oauth2/addclient
+        "client_secret" = "", # Same as client ID
+        "access_token" = "", # You get the access token and refresh token following this: https://api.imgur.com/oauth2
+        "refresh_token" = ""} # Don't worry if you don't have the access_token and refresh_token. This program will guide you (still WIP)
